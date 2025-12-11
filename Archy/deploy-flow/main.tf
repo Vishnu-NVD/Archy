12e1696a12c38310556f86ff1cc57b3cca1a5cf0
Vishnu-NVD <vishnu.muthusamy@servion.com>
2025-12-11 15:35:22 +0530

    Create main.tf

---
+++ Archy/deploy-flow/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    genesyscloud = {
+      source  = "MyPureCloud/genesyscloud"
+      version = "~> 1.70.0"
+    }
+  }
+ }
+
+ resource "genesyscloud_flow" "classifier_flow" {
+  filepath          = "./modules/deploy_flows/EmailComprehendFlow.yaml"
+  file_content_hash = filesha256("./modules/deploy_flows/EmailComprehendFlow.yaml")
+}
