327708d7bd123a022cb06b5fca0e6cbbfd91d83d
Vishnu-NVD <vishnu.muthusamy@servion.com>
2025-12-11 16:33:10 +0530

    Update main.tf

--- Archy/cx-as-code/modules/deploy_flows/main.tf
+++ Archy/cx-as-code/modules/deploy_flows/main.tf
@@ -8,6 +8,6 @@ terraform {
  }
 
  resource "genesyscloud_flow" "classifier_flow" {
-  filepath          = "./deploy-flow/archyflow.yaml"
-  file_content_hash = filesha256("./deploy-flow/archyflow.yaml")
+  filepath          = "./modules/deploy_flows/archyflow.yaml"
+  file_content_hash = filesha256("./modules/deploy_flows/archyflow.yaml")
 }
